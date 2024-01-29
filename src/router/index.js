568d4226055e3b1ccfa005b366a5bebd9a08b3f1
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2024-01-29 16:38:26 +0700

    cai dat trang 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+import NotFoundVue from "@/views/NotFound.vue";
 const routes = [
   {
     path: "/",
     name: "contactbook",
     component: ContactBook,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => NotFoundVue,
   },
 ];
 const router = createRouter({
